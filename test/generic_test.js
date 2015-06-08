f2e4ef894e8fc78207c556d3c6dafeeff1a72420
Muhammad Dadu <muhammad@dadu.co>
2015-06-08 11:00:49 -0700

    Initial Commit

---
+++ test/generic_test.js
@@ -0,0 +1,63 @@
+var core = require('..'),
+	path = require('path'),
+	should = require('should'),
+	request = require('request'),
+	exec = require('child_process').exec;
+
+core.loadMochaConditions();
+
+core.newTest('', {
+	endpoints: [
+		{
+			method: 'all',
+			path: '/*',
+			/** [execute description] */
+			handle: function (req, opts, res, next) {
+				console.log('[MOCK-ADDR]', req.url);
+				return next();
+			}
+		},
+		{
+			method: 'get',
+			path: '/test',
+			/** [execute description] */
+			handle: function (req, opts, res) {
+				res.status(200).type('application/json').send({
+					success: true
+				});
+			}
+		}
+	]
+}, function (err, plugin) {
+	var appc = this.appc,
+		tmpDir = this.tmpDir,
+		async = appc.async;
+
+	describe('OS restrictive conditional tests', function () {
+		it.mac('mac only condition', function () {
+
+		});
+
+		it.linux('linux only condition', function () {
+
+		});
+
+		it.win('win only condition', function () {
+
+		});
+	});
+
+	describe('express test', function () {
+		it('endpoint /test', function (done) {
+			request({
+				method: 'get',
+				url: 'http://127.0.0.1:8118/test',
+				json: true
+			}, function (err, req, res) {
+				should.not.exist(err);
+				should(res).have.property('success');
+				return done();
+			});
+		});
+	});
+});
