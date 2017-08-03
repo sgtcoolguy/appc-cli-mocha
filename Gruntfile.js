bb4ab95396aba1bbe0353b1822077b5972195b6f
Christopher Williams <chris.a.williams@gmail.com>
2017-08-03 14:59:12 -0400

    - Bump version to 0.0.6 - Use Jenkinsfile for build

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,51 +7,44 @@ module.exports = function(grunt) {
 				APPC_CLI_TITANIUM_TEST: '1'
 			}
 		},
-		mochaTest: {
-			options: {
-				timeout: 3000,
-				reporter: 'spec',
-				ignoreLeaks: false,
-				globals: [
-					'_key',
-					'requestSSLInitializing',
-					'requestSSLInsideHook',
-					'requestSSLInitialized',
-					'requestSSLHooks',
-					'requestSSLFingerprints'
-				]
-			},
-			src: ['test/*_test.js']
-		},
-		appcJs: {
-			options: {
-				force: true
-			},
-			src: ['lib/**/*.js', 'test/**/*_test.js']
-		},
-		kahvesi: {
-			src: ['test/*_test.js']
-		},
-		appcCoverage: {
-			default_options: {
-				src: 'coverage/lcov.info',
-				force: true
+		mocha_istanbul: {
+			coverage: {
+				src: ['test/*_test.js'],
+				options: {
+					timeout: 3000,
+					ignoreLeaks: false,
+					reporter: 'mocha-jenkins-reporter',
+					reportFormats: ['lcov', 'cobertura'],
+					globals: [
+						'_key',
+						'requestSSLInitializing',
+						'requestSSLInsideHook',
+						'requestSSLInitialized',
+						'requestSSLHooks',
+						'requestSSLFingerprints'
+					]
+				}
 			}
 		},
-		clean: ['tmp']
+		appcJs: {
+			check: {
+				options: {
+					force: true
+				},
+				src: ['lib/**/*.js', 'test/**/*_test.js']
+			}
+		},
+		clean: {
+			test: ['tmp'],
+			cover: ['coverage']
+		},
 	});
 
 	// Load grunt plugins for modules
-	grunt.loadNpmTasks('grunt-mocha-test');
 	grunt.loadNpmTasks('grunt-appc-js');
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-env');
-	grunt.loadNpmTasks('grunt-kahvesi');
-	grunt.loadNpmTasks('grunt-appc-coverage');
+	grunt.loadNpmTasks('grunt-mocha-istanbul');
 
-	// register tasks
-	var coverTasks = ['env', 'kahvesi']
-		.concat(process.env.TRAVIS ? ['appcCoverage', 'clean'] : ['clean']);
-	grunt.registerTask('cover', coverTasks);
-	grunt.registerTask('default', ['appcJs', 'env', 'mochaTest', 'clean']);
+	grunt.registerTask('default', ['clean:test', 'appcJs:check', 'env:dev', 'mocha_istanbul:coverage', 'clean:test']);
 };
---
+++ Jenkinsfile
@@ -0,0 +1,5 @@
+library 'pipeline-library'
+
+buildNPMPackage {
+	
+}
